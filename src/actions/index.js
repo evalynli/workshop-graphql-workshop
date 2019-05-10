ea1661bb2442bd49b8497743c709479fe2900661
Evalyn <evalynshl@gmail.com>
2019-05-09 23:56:18 -0400

    finished wrkshop

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
-// import your npm packages
+import ApolloClient from 'apollo-boost';
+import dotenv from 'dotenv';
+import { GetRepos, AddStar, RemoveStar } from './operations';
+
+dotenv.config();
+
 
 // keys for actiontypes
 export const ActionTypes = {
@@ -8,24 +13,64 @@ export const ActionTypes = {
 };
 
 const GITHUB_API = 'https://api.github.com/graphql';
-const API_KEY = 'YOUR_KEY';
 
 // initialize ApolloClient here
+const client = new ApolloClient({
+  uri: GITHUB_API,
+  headers: { authorization: `bearer ${process.env.TOKEN}` },
+});
 
 export function fetchRepos(query) {
   return (dispatch) => {
-    // fetchRepos query
+    client.query({
+      query: GetRepos,
+      variables: {
+        queryString: query,
+      },
+      fetchPolicy: 'no-cache',
+    })
+      .then((response) => {
+        const repos = response.data.search.edges[0].node.repositories.edges.map(repo => repo.node);
+        dispatch({ type: ActionTypes.FETCH_REPOS, payload: repos });
+      })
+      .catch((error) => {
+        dispatch({ type: ActionTypes.ERROR_SET, error });
+      });
   };
 }
 
 export function addStar(repoID, searchTerm) {
   return (dispatch) => {
-    // addStar mutation
+    client.mutate({
+      mutation: AddStar,
+      variables: {
+        id: repoID,
+      },
+      fetchPolicy: 'no-cache',
+    })
+      .then((res) => {
+        dispatch(fetchRepos(searchTerm));
+      })
+      .catch((error) => {
+        dispatch({ type: ActionTypes.ERROR_SET, error });
+      });
   };
 }
 
 export function removeStar(repoID, searchTerm) {
   return (dispatch) => {
-    // removeStar mutation
+    client.mutate({
+      mutation: RemoveStar,
+      variables: {
+        id: repoID,
+      },
+      fetchPolicy: 'no-cache',
+    })
+      .then((response) => {
+        dispatch(fetchRepos(searchTerm));
+      })
+      .catch((error) => {
+        dispatch({ type: ActionTypes.ERROR_SET, error });
+      });
   };
 }
